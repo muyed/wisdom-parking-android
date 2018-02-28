5900f8f8001731d1cef29e8eb5727dde57d72512
张林 <zhanglin@chexingzhihui.com>
2018-03-01 00:28:57 +0800

    new: thirdplatform login&share

---
+++ app/src/main/java/com/cn/climax/wisdomparking/WBShareActivity.java
@@ -0,0 +1,13 @@
+package com.cn.climax.wisdomparking;
+
+import com.cn.climax.i_carlib.platform.sina.WBShareCallbackActivity;
+
+/**
+ * author：leo on 2018/2/28 23:57
+ * email： leocheung4ever@gmail.com
+ * description: TODO
+ * what & why is modified:
+ */
+
+public class WBShareActivity extends WBShareCallbackActivity {
+}
--- i-carlib/src/main/java/com/cn/climax/i_carlib/share/download/DefaultImageDownloader.java
+++
@@ -1,9 +0,0 @@
-package com.cn.climax.i_carlib.share.download;
-
-public class DefaultImageDownloader extends AbsImageDownloader {
-
-    @Override
-    protected void downloadDirectly(String imageUrl, String filePath, OnImageDownloadListener listener) {
-        new DefaultImageDownLoadTask(imageUrl, filePath, listener).start();
-    }
-}
